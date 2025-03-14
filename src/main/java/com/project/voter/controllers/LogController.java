d11dc007c169d3bd90d6ca1a5837a46b2745ea90
tarkoman4ik <tarkoman4ik@vk.com>
2025-03-14 16:00:41 +0500

    added ContextPropagation to recovery currentContext in new threads

--- src/main/java/com/project/voter/controllers/LogController.java
+++ src/main/java/com/project/voter/controllers/LogController.java
@@ -1,26 +1,29 @@
 package com.project.voter.controllers;
 
+import io.micrometer.context.ContextSnapshot;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
 @Slf4j
 @RequestMapping("/api/logs")
 public class LogController {
-    
+
     @GetMapping("/")
     public String logs() {
         log.info("Some info");
+        ContextSnapshot snapshot = ContextSnapshot.captureAll();
         Runnable log1 = () -> {
+            ContextSnapshot.Scope scope = snapshot.setThreadLocals();
             log.warn("Some warn");
         };
         Runnable log2 = () -> {
+            ContextSnapshot.Scope scope = snapshot.setThreadLocals();
             log.error("Some error");
         };
         Thread th1 = new Thread(log1);
@@ -36,9 +39,8 @@ public class LogController {
     public void exceptingLogs() {
         try {
             throw new ArithmeticException();
-        }
-        catch (Exception e){
-            log.error("Caught exception "+e);
+        } catch (Exception e) {
+            log.error("Caught exception " + e);
         }
     }
 }
